7ac36559103c926ccbdccf093a282d772116f868
ksqeib <ksqeib@qq.com>
2026-01-25 16:09:58 +0800

    更新mixin部分说明

--- hyperapi/src/main/java/fun/iiii/openvelocity/mixin/trans/MixinVelocity.java
+++ hyperapi/src/main/java/fun/iiii/openvelocity/mixin/trans/MixinVelocity.java
@@ -13,7 +13,9 @@ public class MixinVelocity {
     @Inject(method = "<init>", at = @At("RETURN"))
     private void onConstruction(CallbackInfo ci) {
         Logger testLogger=LogManager.getLogger(VelocityServer.class);
-        testLogger.info("Hello World!");
-        System.out.println("HTT");
+        testLogger.info("[HyperVC]如有问题请提交issue或加QQ群反馈 群号：946864759");
+        testLogger.info("[HyperVC]此为闭源项目，未授权任何形式二次分发，如果您在非官方群下载到此插件，请反馈给官方");
+        testLogger.info("[HyperVC]如有违规使用，官方将采取必要的法律手段");
+        testLogger.info("[HyperVC]made by 庆灵(QQ:2388990095)");
     }
 }
